26963d7b7c820ce018bf3861141918003101bfb2
criminologylsf <djon1999@me.com>
2021-02-20 09:01:57 +0000

    Second Page Added

--- js/index.20210220090053.js
+++ js/index.20210220090053.js
@@ -3,13 +3,13 @@ $.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof att
 
 
 $(document).ready(function() {
-r=function(){dpi=window.devicePixelRatio;$('.js-3').attr('src', (dpi>1) ? 'images/pasted-image-286.jpg' : 'images/pasted-image-143.jpg');};
+r=function(){dpi=window.devicePixelRatio;$('.js-12').attr('src', (dpi>1) ? 'images/pasted-image-286.jpg' : 'images/pasted-image-143.jpg');};
 if(!window.HTMLPictureElement){r();}
 (function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
 var wl = new woolite();
 wl.init();
-wl.addAnimation($('.js-4')[0], "1.00s", "0.00s", 1, 100);
-wl.addAnimation($('.js-5')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-9')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-10')[0], "1.00s", "0.00s", 1, 100);
 wl.start();
 
 });
