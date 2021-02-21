0107971c2c727bf7261f6a0b50749c73aea2533c
criminologylsf <djon1999@me.com>
2021-02-21 10:53:46 +0000

    External links

--- js/unit_2.20210221105207.js
+++ js/unit_2.20210221105207.js
@@ -3,12 +3,14 @@ $.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof att
 
 
 $(document).ready(function() {
-r=function(){dpi=window.devicePixelRatio;$('.js-16').attr('src', (dpi>1) ? 'images/pasted-image-286-2.jpg' : 'images/pasted-image-143-2.jpg');};
+r=function(){dpi=window.devicePixelRatio;$('.js-18').attr('src', (dpi>1) ? 'images/pasted-image-286.jpg' : 'images/pasted-image-143.jpg');};
 if(!window.HTMLPictureElement){r();}
 (function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
 var wl = new woolite();
 wl.init();
 wl.addAnimation($('.js-14')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-15')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-16')[0], "1.00s", "0.00s", 1, 100);
 wl.start();
 
 });
