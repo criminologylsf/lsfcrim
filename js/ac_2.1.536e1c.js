91f5d7493f61b9e64e2b51b02d86783e8aadb663
criminologylsf <djon1999@me.com>
2021-06-16 15:19:25 +0100

    1.1.2

--- js/ac_2.1.536e1c.js
+++ js/ac_2.1.536e1c.js
@@ -7,5 +7,6 @@ $(function(){
 r=function(){dpi=window.devicePixelRatio;var e=document.querySelector('.js33');e.setAttribute('src',(dpi>1)?'images/pasted-image-122-3.png':'images/pasted-image-61-1.png');};
 if(!window.HTMLPictureElement){r();}
 (function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(i,e){$(e).click(function(){var t=e.hash.length>1?$('[name="'+e.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+initMenu($('#m6')[0]);
 
 });
--- js/unit-1.536e1c.js
+++ js/unit-1.536e1c.js
@@ -7,5 +7,6 @@ $(function(){
 r=function(){dpi=window.devicePixelRatio;};
 if(!window.HTMLPictureElement){r();}
 (function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(i,e){$(e).click(function(){var t=e.hash.length>1?$('[name="'+e.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+initMenu($('#m3')[0]);
 
 });
